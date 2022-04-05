ae387d159b3e82a89e8a8b338dd59dafeb45a336
GitMrYu <100071487+GitMrYu@users.noreply.github.com>
2022-04-05 11:42:38 +0800

    Add files via upload

---
+++ Parkour/Parkour.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <easy2d/easy2d.h>
+using namespace easy2d;
+
+int main(){
+	if (Game::init()){
+		std::cout << " Initialize the success!" << std::endl;
+		Window::setTitle(L"窗口名称");
+		Window::setSize(窗口x, 窗口y);
+		//以下是游戏代码
+		
+
+		//以上是游戏代码
+		Game::start;
+	}
+	Game::destroy();
+    return 0;
+}
